b0603c2f6ed97ebd4af0d3daa010f03c59bb3e50
Abhimanyu <lp.abhimanyu@gmail.com>
2023-09-22 17:34:19 +0530

    Added user login, register and logout(blacklist) routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,58 @@
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const { userModel } = require("../models/user.model")
+const { blacklistModel } = require("../models/blacklist.model")
 
-const login = async () => {
+const login = async (req, res) => {
+    const { email, password } = req.body
     try {
-        
+        const user = await userModel.findOne({ email })
+        if (user) {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result) {
+                    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.privateKey, { expiresIn: '3h' });
+                    res.status(200).send({ "msg": "Signin Success!", token, userID: user._id })
+                } else {
+                    res.status(400).send({ "msg": "Incorrect Password" })
+                }
+            });
+        } else {
+            res.status(404).send({ "msg": "No user found with given email" })
+        }
     } catch (error) {
-        
+        res.status(400).send({ "msg": error.message })
     }
 }
 
-const register = async () => {
+const register = async (req, res) => {
+    const { name, email, password, role } = req.body
     try {
-        
+        const user = await userModel.findOne({ email })
+
+        if (user) {
+            res.status(400).send({ "msg": "User already exist!" })
+        } else {
+            bcrypt.hash(password, 3).then(async (hash) => {
+                const user = new userModel({ name, email, password: hash, role })
+                await user.save()
+                res.status(200).send({ "msg": "Registeration completed!" })
+            });
+        }
     } catch (error) {
-        
+        res.status(400).send({ "msg": error.message })
     }
 }
 
-module.exports = { login, register }
+const logout = async (req, res) => {
+    const { token } = req.params;
+    console.log(token)
+    try {
+        let newBlacklistToken = new blacklistModel({ token: token });
+        await newBlacklistToken.save();
+        return res.send({ "msg": "Logout successful" });
+    } catch (error) {
+        res.status(400).send({ "msg": error.message });
+    }
+}
+
+module.exports = { login, register, logout }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const userRouter = express.Router()
-const { login, register } = require("../controllers/user.controller")
+const { login, register, logout } = require("../controllers/user.controller")
 
 userRouter.post("/login", login)
 userRouter.post("/register", register)
+userRouter.post("/logout/:token", logout)
 
 module.exports = { userRouter }
